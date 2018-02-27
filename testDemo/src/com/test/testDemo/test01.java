09f9eca9100654e8c60847856e5e51d812db036a
ichicony <545813550@qq.com>
2018-02-27 10:54:34 +0800

    ag

--- testDemo/src/com/test/testDemo/test01.java
+++ testDemo/src/com/test/testDemo/test01.java
@@ -17,7 +17,7 @@ public class test01 {
     private ApplicationContext ctx = null;
     //test
     public void update(){
-    	System.out.println("pull");
+    	System.out.println("pullTestAgin");
     }
     @Before
     public void doBefore(){
